0c5bfb956d5d080c223fba26fb62c9c1bf099cba
AshishJainMSRIT <ashishjainkv@gmail.com>
2019-11-29 16:02:13 +0530

    Add files via upload

---
+++ myflaskapp/data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles= [
+        {
+            'id': 1,
+            'title':'Article one',
+            'body':'vjkfhjfdhjgsdfkjhj sdfs ',
+            'author':'ashish',
+            'create date':'04-02-2019'
+        },
+        {
+            'id': 2,
+            'title':'Article one',
+            'body':'vjkfhjfdhjgsdfkjhj sdfs ',
+            'author':'ashish',
+            'create date': '12-02-2019'
+        }
+    ]
+    return articles
